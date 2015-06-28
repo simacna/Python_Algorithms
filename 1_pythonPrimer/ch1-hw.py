3fd0f1d16673228aff1af402368ce176760481dc
simacna <simacna@gmail.com>
2015-06-28 19:12:59 -0400

    submmiting some problems

--- 1_pythonPrimer/ch1-hw.py
+++ 1_pythonPrimer/ch1-hw.py
@@ -8,3 +8,17 @@ def is_multiple(n,m):
 		return True
 	else:
 		return False
+
+# R1.2 - Write a short Python function, is_even(k), that takes an integer value and returns True if k is even, and False otherwise.
+# However, your function cannot use the multiplication, modulo, or division operator.
+
+def is_even(k):
+	try: 
+        return int(k) & 1 == 0 
+    except ValueError: 
+        print(”Number must be Integer values”)
+
+
+# R1.3 - Write a short Python function, minmax(data), that takes a sequence of one or more numbers and returns the smallest
+# and largest numbers, in the form of a tuple of length two. Do not use the built-in functions min or max in implementing
+# your solution. 
