bf09353e6f15e94fcef0954b7e017ce6ed4686d0
simacna <simacna@gmail.com>
2015-07-02 23:32:20 -0400

    commiting before continuing hw

--- 1_pythonPrimer/ch1-hw.py
+++ 1_pythonPrimer/ch1-hw.py
@@ -22,3 +22,6 @@ def is_even(k):
 # R1.3 - Write a short Python function, minmax(data), that takes a sequence of one or more numbers and returns the smallest
 # and largest numbers, in the form of a tuple of length two. Do not use the built-in functions min or max in implementing
 # your solution. 
+
+def minmax(*args):
+	return args
