55892a2eaa7e8e168aded750a1d8da25ba5ca441
Sina Sima <simacna@gmail.com>
2015-08-17 21:29:22 -0400

    adding progression method

--- 2_OOP/objects.py
+++ 2_OOP/objects.py
@@ -260,6 +260,17 @@ class PredatoryCreditCard(CreditCard):
             self._balance *= monthly_factor
 
 
+class Progression:
+    """Iterator producing a generic progression.
+
+    Default iterator produces the whole numbers 0, 1, 2 ...
+    """
+
+    def __init__(self, start=0):
+        """Initialize current to the first value of the progression."""
+        self._current = start
+
+
 
 
 
