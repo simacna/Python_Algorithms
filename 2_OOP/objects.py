2f2416d70bf0d889713e6c9a09b2a99316ca5cdc
Sina Sima <simacna@gmail.com>
2015-08-17 20:34:46 -0400

    implemented own Range class

--- 2_OOP/objects.py
+++ 2_OOP/objects.py
@@ -154,6 +154,8 @@ class SequenceIterator:
     def __next__(self):
         """Return the next element, or else raise StopIteration error."""
         self._k += 1
+        print "self._k", self._k #adding this to see what state self._k is in
+
         if self._k < len(self._seq):
             return(self._seq[self._k])
         else:
@@ -162,6 +164,48 @@ class SequenceIterator:
     def __iter__(self):
         """By convention, an iterator must return as an iterator."""
         return self
+
+
+#Above does keep looping through as below example shows:
+
+seq = SequenceIterator([2,3,5])
+print seq.__next__() #2
+print seq.__next__() #3
+print seq.__next__() #5
+
+#Below we implement out own range method
+
+class Range:
+
+    def __init__(self, start, stop=None, step=1):
+        """Initializes a Range instance.
+        Semantics is simlar to build-in range class"""
+
+        if step == 0:
+            raise ValueError('step cannot be 0')
+        if stop is None:
+            start, stop = 0, start #should be treaated as range(0, n)
+
+        #calculate the effective length once
+        self._length = max(0, (stop - start + step -1)//step)
+        #need knowlede of start and step (but not stop) to support __getitem__
+        self._start = start
+        self._step = step
+
+    def __len__(self):
+        """Return number of entries in the range."""
+        return self._length
+    def __getitem__(self, k):
+        """Return entry at index k (using standard interpretation if negative)"""
+        if k<0:
+            k+= len(self) #attempt to convert negative index
+
+        if not 0 <= k < self._length:
+            raise IndexErro('index out of range')
+
+        return self._start + k * self._step
+
+
 
             
 
