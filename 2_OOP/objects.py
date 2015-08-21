ecbb77bd082f7720cff6a1b44545095b6ade00bc
Sina Sima <simacna@gmail.com>
2015-08-21 14:30:44 -0400

    created subclass for progression base class and override a method

--- 2_OOP/objects.py
+++ 2_OOP/objects.py
@@ -343,6 +343,26 @@ class Progression:
 
 
 
+#An arithmetic progression class - while the default progression increases its value by one in each step, an arithmetic
+#progression adds a fixed constant to one term of the progression to the next. 
+
+class ArithmeticProgression(Progression):
+    """Iterator producing an arithmetic progression"""
+
+    def __init__(self, increment=1, start=0):
+        """ Create a new arithmetic progression
+        increment the fixed constant to add to each term (default 1)
+        start     the first term of the progression
+        """
+
+        super().__init__(start) #call super constructor/initialize base class
+        self._increment = increment
+
+    def _advance(self): #override inherited version
+        """ Update current value by adding the fixed increment"""
+        self._current += self._increment
+
+
 
 
 
