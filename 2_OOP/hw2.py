d80eb94a03095fbc913997c8c631cc314412fb77
Sina Sima <simacna@gmail.com>
2015-09-23 13:43:18 -0400

    continuing problems

--- 2_OOP/hw2.py
+++ 2_OOP/hw2.py
@@ -5,16 +5,16 @@
 
 class Flower:
 
-	def __init__(self, name, numberOfPetals, price):
-		self.name = name
-		self.numberOfPetals = numberOfPetals
-		self.price = price
+    def __init__(self, name, numberOfPetals, price):
+        self.name = name
+        self.numberOfPetals = numberOfPetals
+        self.price = price
 
-	def setName(name):
-		self.name = name
-		return type(self.name), self.name #if currently I take out the return self.name and run the below code below, the correct answer will 
-		#still be returned... why? Is it changing the constructor?
-		#also return type doesn't return the type, why
+    def setName(self, name):
+        self.name = name
+        return type(self.name), self.name #if currently I take out the return self.name and run the below code below, the correct answer will
+        #still be returned... why? Is it changing the constructor?
+        #also return type doesn't return the type, why
 
 
     def setPetals(self, numOfPets):
@@ -30,10 +30,69 @@ class Flower:
 
 new = Flower('rose', 5, 4)
 new.setName("sun")
-print(new.name)
+# print(new.name)
 #The above code works except for the return type of the values
 
 #R2.5 - Use the techniques of Section 1.7 to revise the charge and make_payment methods of the CreditCard class to ensure that
 #the caller sends a number as a parameter
 
+class CreditCard:
+    """A consumer credit card"""
+
+    def __init__(self, customer, bank, acnt, limit):
+        """ Create a new credit card instance.
+        The initial balance is zero.
+        customer  the name of the customer (e.g. 'John Bowman')
+        bank      the name of the bank (e.g. 'California Savings')
+        acnt      the account identifier (e.g. '5391 0375 9387 5309')
+        limit     credit limit (measured in dollars)
+        """
+
+        self._customer = customer
+        self._bank = bank
+        self._account = acnt
+        self._limit = limit
+        self._balance = 0
+
+    def get_customer(self):
+        """ Return name of customer. """
+        return self._customer
+
+    def get_bank(self):
+        """Return the bank's name."""
+        return self._bank
+
+    def get_account(self):
+        """ Return the card identifying number (typically stored as a string)."""
+        return self._account
+
+    def get_limit(self):
+        """Return current credit limit"""
+        return self._limit
+
+    def get_balance(self):
+        """Return current balance."""
+        return self._balance
+
+    def charge(self, price):
+        """ Charge given price to the card, assuming sufficient credit limit.
+        Return True if charge was processed; False if charge was denied."""
+
+        # if price + self._balance > self._limit:
+        #     return False
+        # else:
+        #     self._balance += price
+        #     return True
+        if not isinstance(price, (int,float)):
+            raise TypeError('Price must be an integer')
+        else:
+            if price + self._balance > self._limit:
+                return False
+            else:
+                self._balance += price
+                return True
+
+    def make_payment(self, amount):
+        """ Process customer payment that reduces balance."""
+        return self._balance -= amount
 
