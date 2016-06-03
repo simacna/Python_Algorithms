422eecf14d7455198e4d562a0ab11e5d0ac0dc6d
Sina Sima <simacna@gmail.com>
2016-06-03 16:37:43 -0400

    continuing chapter 3

--- 3_algorithmAnalysis/algoanalysis.py
+++ 3_algorithmAnalysis/algoanalysis.py
@@ -3,12 +3,14 @@
 # 3.1 - Experimental studies
 
 from time import time
-start_tinme = time()
-run algorithm
-end_time = time()
-elapsed = end_time - start_time
+# start_tinme = time()
+# # run algorithm
+# end_time = time()
+# elapsed = end_time - start_time
 
 # Because many processes share use of a computer central processing unit (CPU), the elapsed time will depend on what other
 # processes are running on the computer when the test is performed. 
 # A fairer metric is the number of CPU cycles that used by the algorithm. This can be determined using the clock function of the time
 # module, but even this measure might not be consistent if repeating the identical algo on the identical input. 
+
+print("yo")
