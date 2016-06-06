af27aeb6843d75dff6f1c25ff57dafcc095c0c8c
TED <ted@Sina-Simas-Macbook-Air.local>
2016-06-06 11:42:38 -0400

    testing

--- 3_algorithmAnalysis/algoanalysis.py
+++ 3_algorithmAnalysis/algoanalysis.py
@@ -11,6 +11,7 @@ from time import time
 # Because many processes share use of a computer central processing unit (CPU), the elapsed time will depend on what other
 # processes are running on the computer when the test is performed. 
 # A fairer metric is the number of CPU cycles that used by the algorithm. This can be determined using the clock function of the time
-# module, but even this measure might not be consistent if repeating the identical algo on the identical input. 
+# module, but even this measure might not be consistent if repeating the identical algo on the identical input
 
 print("yo")
+
