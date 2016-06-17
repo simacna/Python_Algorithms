e32be48c7cee911bfd5478e9d001911d58efcfde
Sina Sima <simacna@gmail.com>
2016-06-17 10:27:07 -0400

    pushing before continuing

--- 3_algorithmAnalysis/algoanalysis.py
+++ 3_algorithmAnalysis/algoanalysis.py
@@ -103,6 +103,14 @@ def disjoint(A, B, C):
 					return False
 	return True
 
+def disjoint2(A, B, C):
+	for a in A:
+		for b in B:
+			if a == b:
+				for c in C:
+					if a == c:
+						return False
+	return True
 
 
 
