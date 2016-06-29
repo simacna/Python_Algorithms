1a472640dea1c751f49b3654d846581ca5b4ecab
Sina Sima <simacna@gmail.com>
2016-06-29 10:33:40 -0600

    finishing ch 3

--- 3_algorithmAnalysis/algoanalysis.py
+++ 3_algorithmAnalysis/algoanalysis.py
@@ -113,6 +113,16 @@ def disjoint2(A, B, C):
 						return False
 	return True
 
+	#element uniquness problem
+
+	def unique1(S):
+		""" Return True if there are no duplicate elements in sequence S"""
+		for j in range(len(S)):
+			for k in range(j+1, len(S)):
+				if S[j] == S[k]:
+					return False
+		return True
+
 
 
 
