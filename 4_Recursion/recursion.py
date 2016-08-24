aff0c51e7cace0873642742b45a8267562da708c
Sina Sima <simacna@gmail.com>
2016-08-24 16:55:47 -0400

    some ch 4 algo

--- 4_Recursion/recursion.py
+++ 4_Recursion/recursion.py
@@ -8,3 +8,26 @@ def rec(n):
   
 
 print(rec(3))
+
+
+#below is where 3 functions related to drawing ruler are
+
+def draw_line(tick_length, tick_label=""):
+  """Draw one line with given tick length (followed by optional hash label)"""
+  line = '-' * tick_length
+  print(line)
+
+def draw_interval(center_length):
+  """Draw tick interval upon a central tick length"""
+  if center_length > 0:
+    draw_interval(center_length - 1)
+    draw_line(center_length)
+    draw_interval(center_length - 1)
+
+def draw_ruler(num_inches, major_length):
+  """Draw English ruler with given number of inches, major tick length"""
+  draw_line(major_length, '0')
+  for j in range(1, 1 + num_inches):
+    draw_line()
+
+
